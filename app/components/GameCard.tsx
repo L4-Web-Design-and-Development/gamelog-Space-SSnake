cf6e2cab01a60e0629f45f161683025984d36af8
Space-SSnake <1143175@hc.ac.je>
2025-05-08 10:07:27 +0100

    Done titlte and realsease Date

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,5 +1,11 @@
+interface GameCardProps {
+  title: string;
+  releaseDate: string;
+}
+
 import witcherLogo from "~/assets/svg/Witcher.png";
-export default function GameCard() {
+export default function GameCard(props: GameCardProps) {
+  const formattedDate = props.releaseDate.slice(0, 10);
   return (
     <div className="flex flex-col w-1/3 py-10 rounded-lg">
       <div className="py-10 rounded-full">
@@ -7,12 +13,12 @@ export default function GameCard() {
       </div>
       <div className="flex justify-between">
         <div className="flex flex-col gap-4">
-          <div className="text-2xl font-bold">Witcher 3: Wild Hunt</div>
-          <div className="text-cyan-400">RPG</div>
-          <div className="text-gray-400">2023-10-01</div>
+          <div className="text-2xl font-bold">{props.title}</div>
+          <div className="text-cyan-300">RPG</div>
+          <div className="text-gray-400">{formattedDate}</div>
         </div>
         <div className="flex flex-col justify-between gap-6 w-90">
-          <button className="   flex items-center justify-center bg-transparent  border-2 border-cyan-400  text-cyan-400 px-4 py-2 text-sm font-bold rounded hover:bg-green-100 w-full h-7">
+          <button className="   flex items-center justify-center bg-transparent  border-2 border-cyan-300  text-cyan-300 px-4 py-2 text-sm font-bold rounded hover:bg-green-100 w-full h-7">
             Edit
           </button>
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client'
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { PrismaClient } from "@prisma/client";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import type { MetaFunction } from "@remix-run/node";
-import GameCard from '~/components/GameCard';
+import GameCard from "~/components/GameCard";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,27 +12,27 @@ export const meta: MetaFunction = () => {
 };
 
 export async function loader() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
-  const games = await prisma.game.findMany()
+  const games = await prisma.game.findMany();
 
-  return json({ games })
+  return json({ games });
 }
 
 export default function Index() {
-  const { games } = useLoaderData<typeof loader>()
+  const { games } = useLoaderData<typeof loader>();
 
-  console.log({ games })
+  console.log({ games });
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div>
-        <h1 className="text-4xl font-bold">Hello, world!</h1>
-        <GameCard/>
         {games.map((game) => (
-          <div key={game.id}>
-            <h2>{game.title}</h2>
-          </div>
+          <GameCard
+            key={game.id}
+            title={game.title}
+            releaseDate={game.releaseDate}
+          />
         ))}
       </div>
     </div>
